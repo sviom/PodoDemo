ba16ed63196b13ce2728cc4332638c5c0f94f59f
HanByulKang <kanghanstar@outlook.com>
2017-06-30 16:48:05 +0900

    에러  API 관련 게시 /수정

--- ErrorAPI/Controllers/ErrorController.cs
+++ ErrorAPI/Controllers/ErrorController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ErrorAPI.Common;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -34,6 +36,53 @@ namespace ErrorAPI.Controllers
         // DELETE api/<controller>/5
         public void Delete(int id)
         {
+        }
+
+        /// <summary>
+        /// DB Connection 세팅
+        /// </summary>
+        [ActionName("SetDBConnection")]
+        public bool SetDBConnection(string id = "")
+        {
+            bool setResult = false;
+            
+            switch (id)
+            {
+                case "podocm":
+                    CommonData.connectionString = ConfigurationManager.ConnectionStrings["podocmDB"].ToString();
+                    break;
+                case "pododemo":
+                    CommonData.connectionString = ConfigurationManager.ConnectionStrings["podoDemoDB"].ToString();
+                    break;
+                default:
+                    break;
+            }
+
+            return setResult;
+        }
+
+        /// <summary>
+        /// 현재 설정되어 있는 DB Connection String 확인
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [ActionName("GetConnectedString")]
+        public string GetConnectedString(string id)
+        {
+            string connectionString = "ERROR";
+            switch (id)
+            {
+                case "podocm":
+                    connectionString = ConfigurationManager.ConnectionStrings["podocmDB"].ToString();
+                    break;
+                case "pododemo":
+                    connectionString = ConfigurationManager.ConnectionStrings["podoDemoDB"].ToString();
+                    break;
+                default:
+                    break;
+            }
+
+            return connectionString;
         }
     }
 }
---
+++ PodoDemo/Models/Error.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PodoDemo.Models
+{
+    public class Error
+    {
+        public long Id { get; set; }
+        public string Errortitle { get; set; }
+        public string Errorcontent { get; set; }
+        public string Createuser { get; set; }
+        public DateTime Createdate { get; set; }
+    }
+}
